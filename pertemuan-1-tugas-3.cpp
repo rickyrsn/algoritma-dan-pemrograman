1d52a352f3c5374bffb363bfc61d569306b923b2
rickyrsn <rickysut96@gmail.com>
2021-04-18 22:51:31 +0700

    Tugas 3

--- pertemuan-1-tugas-3.cpp
+++ pertemuan-1-tugas-3.cpp
@@ -3,16 +3,58 @@ using namespace std;
 
 int main()
 {
-    float a, b, c, avg, sum;
-    cout<<"Input Bilangan A :";
-    cin >> a;
-    cout<<"Input Bilangan B :";
-    cin >> b;
-    cout<<"Input Bilangan C :";
-    cin >> c;
+  float a, b, c, avg;
+  cout<<"Input Bilangan A :";
+  cin >> a;
+  cout<<"Input Bilangan B :";
+  cin >> b;
+  cout<<"Input Bilangan C :";
+  cin >> c;
 
-    sum = a + b + c;
-    avg = sum / 3;
+  // Nilai rata-rata
+  avg = (a+b+c)/3;
+  cout<<"Nilai rata-ratanya adalah : "<< avg << endl;
 
-    cout<< avg << endl;
+  // Mencari nilai terbesar dan terkecil
+  if (a > b and a > c)
+  {
+    if (b > c)
+    {
+      cout<<"Bilangan terbesar adalah : " << a << endl;
+      cout<<"Bilangan terkecil adalah : " << c << endl;
+    }
+    else
+    {
+      cout<<"Bilangan terbesar adalah : " << a << endl;
+      cout<<"Bilangan terkecil adalah : " << b << endl;
+    }
+  }
+  
+  if (b > c and b > a)
+  {
+    if (c > a)
+    {
+      cout<<"Bilangan terbesar adalah : " << b << endl;
+      cout<<"Bilangan terkecil adalah : " << a << endl;
+    }
+    else
+    {
+      cout<<"Bilangan terbesar adalah : " << b << endl;
+      cout<<"Bilangan terkecil adalah : " << c << endl;
+    } 
+  }
+
+  if (c > a and c > b)
+  {
+    if (a > b)
+    {
+      cout<<"Bilangan terbesar adalah : " << c << endl;
+      cout<<"Bilangan terkecil adalah : " << b << endl;
+    }
+    else
+    {
+      cout<<"Bilangan terbesar adalah : " << c << endl;
+      cout<<"Bilangan terkecil adalah : " << a << endl;
+    }
+  }
 }
